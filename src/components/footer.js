88f8f72efa2dfbb04fd6c7511479017efc21a801
Fred <73175901+diverdowngit@users.noreply.github.com>
2023-02-14 11:32:46 +0100

    first

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,16 +1,40 @@
-import React from 'react'
-
-import Container from './container'
-import * as styles from './footer.module.css'
-
-const Footer = () => (
-  <Container as="footer">
-    <div className={styles.container}>
-      Built with <a href="https://contentful.com/">Contentful</a> and{' '}
-      <a href="https://gatsbyjs.com">Gatsby</a> &middot;{' '}
-      <a href="https://github.com/contentful/starter-gatsby-blog">Source</a>
+import React from 'react';
+import { Link } from 'gatsby';
+import { AiOutlineInstagram,AiOutlineYoutube } from 'react-icons/ai';
+import { FaFacebookF } from 'react-icons/fa';
+import Logo from '../images/Logo.png';
+import '../styles/layout.module.css'
+import {container,brand,wrapper,logo,brandDescription,socials,socialTitle,social}from '../styles/footer.module.css';
+import Seo from "../components/seo"
+const Footer = () => {
+  return (
+    <div className={container}>
+      <div className={wrapper}>
+    
+        <div className={brand}>
+        <div className={logo}><Link to='/'><img src={Logo} alt="logo of sea senora"/></Link></div>
+          <div className={brandDescription}>
+            The website ends here, but your journey to the beautiful destinations
+             begins with Sv Sea Senora. Explore with us.
+          </div>
+        </div>
+        <div className={socials}>
+          <div className={socialTitle}>social</div>
+          <div className={social}>
+          <a href="https://www.instagram.com/svseasenora/"target="_blank"rel="noopener noreferrer"  ><AiOutlineInstagram /></a>
+            <a href="https://www.youtube.com/channel/UCpaA07tlk3PA44WApp8oKDw"target="_blank" rel="noopener noreferrer"  ><AiOutlineYoutube /></a> 
+            <a href="https://www.facebook.com/profile.php?id=100085674495009" target="_blank" rel="noopener noreferrer" ><FaFacebookF /></a>
+          </div>
+          <div>Copyright © 2022 - {new Date().getFullYear()} SV Sea Senora</div>
+          {/* <div className={copyRights}>
+            SV Sea Senora © 2022.
+            <br /> All Rights Reserved
+          </div> */}
+        </div>
+     
+      </div>
     </div>
-  </Container>
-)
-
+  )
+}
+export const Head = () => <Seo title="footer" />
 export default Footer
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,65 +1,26 @@
-import React from 'react'
-import { graphql } from 'gatsby'
-import get from 'lodash/get'
+import * as React from "react"
+import { Link } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
+import '../styles/layout.module.css'
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+import Img from '../images/heroimage.jpg'
+import Hero from '../components/Hero'
+import Ewv from "../pages/Ewv"
+const IndexPage = () => {
+  return (
+    <Layout>
+  
+     {/* <Hero img={Img} centerText="Welcome to SV Sea Senora " location="WorldWide."font-family=" 'Luminari', sans-serif;"/> */}
+      
+       <Ewv />
+    
 
-import Layout from '../components/layout'
-import Hero from '../components/hero'
-import ArticlePreview from '../components/article-preview'
-
-class RootIndex extends React.Component {
-  render() {
-    const posts = get(this, 'props.data.allContentfulBlogPost.nodes')
-    const [author] = get(this, 'props.data.allContentfulPerson.nodes')
-
-    return (
-      <Layout location={this.props.location}>
-        <Hero
-          image={author.heroImage.gatsbyImage}
-          title={author.name}
-          content={author.shortBio}
-        />
-        <ArticlePreview posts={posts} />
+      
+     
+     
       </Layout>
-    )
-  }
+  )
 }
-
-export default RootIndex
-
-export const pageQuery = graphql`
-  query HomeQuery {
-    allContentfulBlogPost(sort: { publishDate: DESC }) {
-      nodes {
-        title
-        slug
-        publishDate(formatString: "MMMM Do, YYYY")
-        tags
-        heroImage {
-          gatsbyImage(
-            layout: FULL_WIDTH
-            placeholder: BLURRED
-            width: 424
-            height: 212
-          )
-        }
-        description {
-          raw
-        }
-      }
-    }
-    allContentfulPerson(
-      filter: { contentful_id: { eq: "15jwOBqpxqSAOy2eOO4S0m" } }
-    ) {
-      nodes {
-        name
-        shortBio {
-          raw
-        }
-        title
-        heroImage: image {
-          gatsbyImage(layout: CONSTRAINED, placeholder: BLURRED, width: 1180)
-        }
-      }
-    }
-  }
-`
+export const Head = () => <Seo title="Home" />
+export default IndexPage
